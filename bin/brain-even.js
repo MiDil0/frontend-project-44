ef37151903f5a8231accc8a1674e0829357b51a1
Михаил <shadoumaikl@gmail.com>
2023-01-23 10:56:32 +0300

    .

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -44,7 +44,7 @@ export default function brainEvengame() {
         return;
       }
     }
-    i += 1;
+    
   }
 
   console.log(`Congratulations, ${name} ! `);
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,6 +2,7 @@
 import readlineSync from 'readline-sync';
 import brainEven from './brain-even.js';
 import brainCalc from './brain-calc.js';
+import brainGcd from './brain-gcd.js';
 
 const gameName = readlineSync.question('What do you want to play? ');
 console.log(`${gameName}`);
@@ -10,4 +11,7 @@ if (gameName === 'brainEven') {
 }
 if (gameName === 'brainCalc') {
   brainCalc();
+}
+if (gameName === 'brainGcd') {
+  brainGcd();
 }
