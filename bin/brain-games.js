6b12966dc1fe4b1aa320a04c307a81ea7bc820d4
Михаил <shadoumaikl@gmail.com>
2023-01-25 12:05:03 +0300

    .

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,17 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import brainEven from './brain-even.js';
-import brainCalc from './brain-calc.js';
-import brainGcd from './brain-gcd.js';
 
 const gameName = readlineSync.question('What do you want to play? ');
 console.log(`${gameName}`);
-if (gameName === 'brainEven') {
-  brainEven();
-}
-if (gameName === 'brainCalc') {
-  brainCalc();
-}
-if (gameName === 'brainGcd') {
-  brainGcd();
-}
